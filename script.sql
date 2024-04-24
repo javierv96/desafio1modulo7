9b226a40e4696402bcba76cf70f7ad9d132c2ef3
Javier Vergara <ja.vergarac96@gmail.com>
2024-04-23 22:07:08 -0400

    BD script

--- script.sql
+++ script.sql
@@ -3,8 +3,8 @@ create database desafio1modulo7;
 \c desafio1modulo7;
 
 create table estudiantes (
-	nombre VARCHAR(25) NOT NULL,
-	rut VARCHAR(14) PRIMARY KEY,
-	curso VARCHAR(25) NOT NULL,
-	nivel INT NOT NULL
+	Nombre VARCHAR(25) NOT NULL,
+	Rut VARCHAR(14) PRIMARY KEY,
+	Curso VARCHAR(25) NOT NULL,
+	Nivel INT NOT NULL
 );
